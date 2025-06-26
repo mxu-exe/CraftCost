259cd23759151eeda6a003ac3293c4c44f890746
mxu-exe <mxu.exe@gmail.com>
2025-06-26 14:50:54 +0100

    Improve Material suggestions

--- src/main/java/com/umnirium/mc/craftcost/utils/CommandUtils.java
+++ src/main/java/com/umnirium/mc/craftcost/utils/CommandUtils.java
@@ -3,16 +3,40 @@ package com.umnirium.mc.craftcost.utils;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.concurrent.CompletableFuture;
 
 import static com.umnirium.mc.craftcost.CraftCost.plugin;
 
 @SuppressWarnings("UnstableApiUsage")
 public class CommandUtils {
+    private static CompletableFuture<Suggestions> materialSuggestions(CommandContext<CommandSourceStack> context, SuggestionsBuilder builder) {
+        String input = builder.getRemainingLowerCase();
+
+        for (Material material : Material.values()) {
+            try {
+                if (material.isLegacy()) continue;
+
+                new ItemStack(material);
+            } catch (Exception e) {
+                continue;
+            }
+
+            if (material.name().toLowerCase().startsWith(input)) {
+                builder.suggest(material.name().toLowerCase());
+            }
+        }
+
+        return builder.buildFuture();
+    }
 
     public static LiteralCommandNode<CommandSourceStack> createCommand() {
         return Commands.literal("craftcost")
@@ -20,30 +44,14 @@ public class CommandUtils {
                 .then(Commands.literal("normal")
                         .requires(ctx -> ctx.getSender().hasPermission("craftcost.command.normal"))
                         .then(Commands.argument("item", StringArgumentType.word())
-                            .suggests((context, builder) -> {
-                                for (Material material : Material.values()) {
-                                    if (material.isLegacy()) continue;
-
-                                    builder.suggest(material.name().toLowerCase());
-                                }
-
-                                return builder.buildFuture();
-                            })
-                                .executes(CommandUtils::executeNormalCalculation)
+                            .suggests(CommandUtils::materialSuggestions)
+                            .executes(CommandUtils::executeNormalCalculation)
                         )
                 )
                 .then(Commands.literal("recursive")
                         .requires(ctx -> ctx.getSender().hasPermission("craftcost.command.recursive"))
                         .then(Commands.argument("item", StringArgumentType.word())
-                                .suggests((context, builder) -> {
-                                    for (Material material : Material.values()) {
-                                        if (material.isLegacy()) continue;
-
-                                        builder.suggest(material.name().toLowerCase());
-                                    }
-
-                                    return builder.buildFuture();
-                                })
+                                .suggests(CommandUtils::materialSuggestions)
                                 .executes(CommandUtils::executeRecursiveCalculation)
                         )
                 )
